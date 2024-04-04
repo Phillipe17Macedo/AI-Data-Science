b3fc8ed26e826fba1ab8f75e46e3b53f81e36cab
Phillipe <ph.stos.udia@gmail.com>
2024-04-04 19:43:43 -0300

    branch-03

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
 
 export default function App() {
-    return (
-      <View style={styles.container}>
-        <Text>App With Expo</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>App With Expo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
